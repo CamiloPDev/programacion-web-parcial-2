067ab29109b0f69c79c75d000cbd7f73773ec556
CamiloPDev <99335525+CamiloPDev@users.noreply.github.com>
2024-10-19 20:00:06 -0500

    feat: initial scripts

---
+++ src/database/scripts/database.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE adventure;
+
+COMMIT;
---
+++ src/database/scripts/fun-table.sql
@@ -0,0 +1,13 @@
+CREATE TABLE fun (
+    Sport_ID INT AUTO_INCREMENT PRIMARY KEY,
+    Title VARCHAR(200),
+    Overview VARCHAR(200),
+    Regulations VARCHAR(200),
+    Squad VARCHAR(200),
+    Athletes VARCHAR(200),
+    Gear VARCHAR(200),
+    Fan_Base VARCHAR(200),
+    Organizing_Authority VARCHAR(200)
+);
+
+COMMIT;
